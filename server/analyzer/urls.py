004a7caf3c6ed8021dfd534c33f31c75951c5d39
PrathamReddy888 <dprathamreddy3@gmail.com>
2026-07-15 22:02:51 +0530

    Add URL path for job description matching

--- server/analyzer/urls.py
+++ server/analyzer/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import upload_resume, signup, analysis_history
+from .views import upload_resume, match_jd, signup, analysis_history
 
 urlpatterns = [
     path("upload/", upload_resume),
+    path("jd-match/", match_jd),
     path("auth/signup/", signup),
     path("auth/login/", TokenObtainPairView.as_view()),
     path("auth/refresh/", TokenRefreshView.as_view()),
